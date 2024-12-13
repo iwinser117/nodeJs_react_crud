9380e23bcdb10b360901eb2dcf8db86b75273b21
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2024-12-13 03:48:42 -0500

    creacion efectiva en windows del contenedor y front

--- backend/src/models/Role.js
+++ backend/src/models/Role.js
@@ -1,27 +1,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/sequelize');
 
-const Role = sequelize.define('Role', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Role = sequelize.define(
+  'Role',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
+  {
+    tableName: 'Roles',
   }
-});
+);
+
 
 Role.findOrCreateCustom = async function ({ where }) {
-    try {
-      const [role, created] = await sequelize.models.Role.findOrCreate({ where });
-      return role;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
+  try {
+    const [role, created] = await sequelize.models.Role.findOrCreate({ where });
+    return role;
+  } catch (error) {
+    throw error;
+  }
+};
+
 
 module.exports = Role;
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,23 +1,25 @@
-const sequelize = require('../config/sequelize');
 const User = require('./User');
-const Request = require('./Request');
 const Role = require('./Role');
+const Request = require('./Request');
+const sequelize = require('../config/sequelize'); // Asegúrate de que la ruta es correcta
+
+
+User.belongsTo(Role, { foreignKey: 'RoleId' });
+Role.hasMany(User, { foreignKey: 'RoleId' });
 
 const initModels = async () => {
   try {
+    console.log('Sincronizando la base de datos...');
+    await sequelize.sync({ force: true }); // ¡CUIDADO: Elimina datos existentes!
+    console.log('Base de datos sincronizada.');
+
+    console.log('Creando roles predeterminados...');
     await Role.findOrCreateCustom({ where: { name: 'employee' } });
     await Role.findOrCreateCustom({ where: { name: 'administrator' } });
-
-    await sequelize.sync({ force: false });
-    console.log('Database synchronized successfully.');
+    console.log('Roles creados.');
   } catch (error) {
-    console.error('Error synchronizing the database:', error);
+    console.error('Error durante la sincronización:', error);
   }
 };
 
-module.exports = {
-  initModels,
-  User,
-  Request,
-  Role,
-};
+module.exports = { initModels, User, Role, Request };
